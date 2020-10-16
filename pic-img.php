67c169d0384aed54264b98a9763a05748db5e4d4
yy6090 <72988283+yy6090@users.noreply.github.com>
2020-10-16 23:47:07 +0800

    Create pic-img.php

---
+++ pic-img.php
@@ -0,0 +1,18 @@
+<?php
+/*图片防盗链处理代码*/
+error_reporting(0);
+header("Content-Type:image/jpeg");
+$picurl = $_GET['pic'];
+echo npimgcurl($picurl);
+function npimgcurl($url){
+	$ch=curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是	
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	$data=curl_exec($ch);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,5);
+	curl_setopt($ch,CURLOPT_REFERER,$url);	
+	curl_close($ch);
+	return $data;
+}
