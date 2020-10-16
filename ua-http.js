48ba90a18f859d368a7533788494ccf843280306
yy6090 <72988283+yy6090@users.noreply.github.com>
2020-10-17 01:18:57 +0800

    Create ua-http.js

---
+++ ua-http.js
@@ -0,0 +1,22 @@
+/*!UA判断跳转 */
+UA = navigator.userAgent.toLowerCase();
+url = window.location;
+url = url.toString();
+if ((UA.indexOf('iphone') != -1 || UA.indexOf('mobile') != -1 || UA.indexOf('android') != -1 || UA.indexOf('ipad') != -1 || UA.indexOf('windows ce') != -1 || UA.indexOf('ipod') != -1) && UA.indexOf('ipod') == -1) {
+if (url.indexOf("m.") < 0) {
+if (url.indexOf("www.") < 0){
+var index = url.indexOf("http://");
+if (index < 0){
+url = "m." + url;
+}else{
+url = url.replace("http://", "http://m.");
+}
+}else{
+url = url.replace("www.", "m.");
+}
+Go(url);
+}
+}
+function Go(url) {
+window.location = url;
+}
